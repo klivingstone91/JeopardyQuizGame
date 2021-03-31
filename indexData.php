70d902817345c00f132f69b393344299dee6d463
kevin <klivingstone91@gmail.com>
2021-03-31 19:45:44 +0100

    some test data

--- indexData.php
+++ indexData.php
@@ -11,5 +11,9 @@
     function getJson() { //collects json data
         $jsonContents = file_get_contents("./theQuizes/testQuiz.JSON");
         var_dump($jsonContents);
+        $data = json_decode($jsonContents, true);
+        var_dump($data);
+        var_dump($data["quiz"][0]["cat1"][1]["100"][0]); // This leads to the first category and the question
+        echo $data["quiz"][0]["cat1"][1]["100"][0]["q"];
     }
 ?>
