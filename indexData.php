bde5c828d88203706bb90406a9ead94aeb7b10fc
kevin <klivingstone91@gmail.com>
2021-04-07 19:11:57 +0100

    creating simplier Json design for easier coding.

--- indexData.php
+++ indexData.php
@@ -16,9 +16,19 @@
         //var_dump($data["quiz"][0]["cat1"][1]["100"][0]); // This leads to the first category and the question
         //echo $data["quiz"][0]["cat1"][1]["100"][0]["q"];
 
-        $jsonInformation = file_get_contents("./theQuizes/testQuiz2.json");
-        var_dump($jsonInformation);
-        $data2 = json_decode($jsonInformation, true);
-        var_dump($data2["cat"]);
+        //$jsonInformation = file_get_contents("./theQuizes/testQuiz2.json");
+        //var_dump($jsonInformation);
+        //$data2 = json_decode($jsonInformation, true);
+        //var_dump($data2["cat"]);
+
+        $jsonInformation2 = file_get_contents("./theQuizes/testQuiz3.json");
+        //var_dump($jsonInformation2);
+        $data3 = json_decode($jsonInformation2, true);
+        var_dump($data3);
+        var_dump($data3["quizData"]["cat5-q500"]);
+
+
+
+
     }
 ?>
